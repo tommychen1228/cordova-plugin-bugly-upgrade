dda5a6160406658ffe9526fbe5ace1f7b2ec6d40
zhangj0110 <zhangj011017@gmail.com>
2017-01-13 17:37:26 +0800

    添加参数ioskey

--- www/UpgradePlugin.js
+++ www/UpgradePlugin.js
@@ -1,5 +1,5 @@
 module.exports = {
-    initApp: function (androidKey, successHandler, errorHandler) {
-        cordova.exec(successHandler, errorHandler, "UpgradePlugin", "initApp", [androidKey]);
+    initApp: function (androidKey,ioskey, successHandler, errorHandler) {
+        cordova.exec(successHandler, errorHandler, "UpgradePlugin", "initApp", [androidKey,ioskey]);
     }
 };
